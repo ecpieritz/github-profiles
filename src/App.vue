ac519d25537d86811fc8a33e7a78772f8ab08e95
ecpieritz <ecpieritz@gmail.com>
2024-04-16 11:20:53 -0300

    feat: add api to PersonCol

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header />
     <div class="row">
       <div class="col-lg-3 col-12">
-        <PersonCol />
+        <PersonCol :name="user.name" :bio="user.bio" />
       </div>
       <div class="col-lg-9 col-12">
         <GitCol />
@@ -13,24 +13,54 @@
 </template>
 
 <script>
-import PersonCol from './components/PersonCol.vue'
-import Header from './components/Header.vue'
-import GitCol from './components/GitCol.vue'
+import githubApi from "./api/githubApi";
+import PersonCol from "./components/PersonCol.vue";
+import Header from "./components/Header.vue";
+import GitCol from "./components/GitCol.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     PersonCol,
     Header,
-    GitCol
-  }
-}
+    GitCol,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    githubApi
+      .getUser("ecpieritz")
+      .then((response) => {
+        this.user = response.data;
+        this.loadBio("ecpieritz");
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar informações do usuário:", error);
+      });
+  },
+  methods: {
+    loadBio(username) {
+      githubApi
+        .getUserBio(username)
+        .then((bio) => {
+          this.user.bio = bio;
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar a biografia do usuário:", error);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-@import url('./assets/scss/index.scss');
+@import url("./assets/scss/index.scss");
 
-html, body{
+html,
+body {
   overflow-x: hidden;
 }
 </style>
--- src/components/PersonCol.vue
+++ src/components/PersonCol.vue
@@ -1,51 +1,70 @@
+<!-- PersonCol.vue -->
 <template>
   <div class="gp-person">
-    <img class="gp-person__img" src="https://avatars.githubusercontent.com/u/66131506?v=4" alt="">
+    <img class="gp-person__img" :src="profileImageUrl" alt="">
     <div class="gp-person__texts">
-      <h3 class="gp-person__texts__name">Emilyn Pieritz</h3>
-      <p class="gp-person__texts__description">Front-end Developer | Vue.js | React | Next | Javascript | Typescript</p>
+      <h3 class="gp-person__texts__name">{{ name }}</h3>
+      <p class="gp-person__texts__description">{{ bio }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import githubApi from '../api/githubApi';
+
 export default {
   name: 'PersonCol',
   props: {
-    msg: String
+    name: String,
+    bio: String
+  },
+  data() {
+    return {
+      profileImageUrl: '' // Inicialmente, o URL da foto de perfil está vazio
+    };
+  },
+  async created() {
+    try {
+      // Fazendo uma solicitação à API do GitHub para obter as informações do usuário 'ecpieritz'
+      const response = await githubApi.getUser('ecpieritz');
+      // Obtendo o URL da foto de perfil do usuário 'ecpieritz' a partir dos dados recebidos
+      this.profileImageUrl = response.data.avatar_url;
+    } catch (error) {
+      console.error('Erro ao obter informações do usuário:', error);
+    }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-.gp-person{
+.gp-person {
   padding: 0 40px;
   text-align: center;
-  &__img{
+  &__img {
     width: 100%;
     border-radius: 50%;
   }
-  &__texts{
-    &__name{
+  &__texts {
+    &__name {
       margin-top: 30px;
       font-size: 28px;
       font-weight: 700;
     }
-    &__description{
+    &__description {
       font-size: 16px;
       font-weight: 400;
     }
   }
-
 }
-@media(max-width: 996px){
-  .gp-person{
+
+@media(max-width: 996px) {
+  .gp-person {
     text-align: left;
     display: flex;
     align-items: center;
     gap: 20px;
     margin-bottom: 40px;
-    &__img{
+    &__img {
       width: 100px;
       height: 100px;
     }
